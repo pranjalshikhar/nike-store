437267d2428234e0f620b37ee8ad2f9dcfb05191
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2022-12-09 19:33:29 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import React from "react";
+import Cart from "./components/Cart";
+import FlexContent from "./components/FlexContent";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import { heroapi } from "./data/data";
+import Navbar from "./components/Navbar";
+import Sales from "./components/Sales";
+import Stories from "./components/Stories";
+import {
+  heroapi,
+  popularsales,
+  toprateslaes,
+  highlight,
+  sneaker,
+  story,
+  footerAPI,
+} from "./data/data";
 
 const App = () => {
   return (
     <>
-      <main>
+      <Navbar />
+      <Cart />
+      <main className="flex flex-col gap-16 relative">
         <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
       </main>
+      <Footer footerAPI={footerAPI} />
     </>
   );
 };
